2240145dcb3cde13d67c03beda4b8654298826f8
Shaun Mane <shaunmane.sm@gmail.com>
2024-11-13 06:29:03 +0000

    variables

--- main.tf
+++ main.tf
@@ -15,7 +15,9 @@ terraform {
 
 # Cloud Provider
 provider "aws" {
-  region     = "us-east-1"
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 # IAM role with all required permissions
